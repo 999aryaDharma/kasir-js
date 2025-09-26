796476fea0277b772ff9807765be1f0290fdebde
Arya Dharma <145570377+999aryaDharma@users.noreply.github.com>
2025-09-26 10:56:02 +0800

    Updated Header.jsx

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { User, LogOut, Bell, UserCircle, ShoppingCart } from "lucide-react";
+import { User, LogOut, Bell, CircleUser as UserCircle, ShoppingCart } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
