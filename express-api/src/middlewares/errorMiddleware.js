1087825ef88e36080527eaf14d12040da43a7c69
Arya Dharma <aryadharmawm22@gmail.com>
2025-09-26 12:22:14 +0800

    commit ulang

--- express-api/src/middlewares/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-function errorMiddleware(err, req, res, next) {
-	const status = err.statusCode || 500;
-	res.status(status).json({
-		success: false,
-		message: err.message || "Internal Server Error",
-	});
-}
-
-module.exports = errorMiddleware;
--- express-api/src/routes/summaryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getSummary } = require("../controllers/summaryController");
-
-router.get("/", getSummary);
-
-module.exports = router;
